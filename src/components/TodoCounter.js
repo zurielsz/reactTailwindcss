ff86e09c2f6cd5a1d0fc409a7f548c3e3ce3e9e5
zurielsz <uriel.zoe.sanjuanm@gmail.com>
2023-03-06 15:42:52 -0600

    deploy

--- src/components/TodoCounter.js
+++ src/components/TodoCounter.js
@@ -8,7 +8,7 @@ function TodoCounter() {
   return (
     <div className="p-4 w-full text-center text-white bg-gradient-to-b from-black rounded-2xl">
       { totalTodos===0 && <p className="font-bold text-lg text-red-500">Crea tu primer TODO</p> }
-      { totalTodos != completedTodos && <h2 className="font-bold text-lg">Completado {completedTodos} de {totalTodos} TODOs</h2> }
+      { totalTodos !== completedTodos && <h2 className="font-bold text-lg">Completado {completedTodos} de {totalTodos} TODOs</h2> }
       { (totalTodos>0 && totalTodos === completedTodos) && <p className="font-bold text-lg text-center text-green-500">Felicidades TODOs Finalizados</p> }
     </div>
   );
